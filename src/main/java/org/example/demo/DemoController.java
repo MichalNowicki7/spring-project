dca96866b92850f1ece8454956eac75bff25aec5
MichalNowicki7 <now.mic7@wp.pl>
2021-06-04 18:07:02 +0200

    Second commit

--- src/main/java/org/example/demo/DemoController.java
+++ src/main/java/org/example/demo/DemoController.java
@@ -3,27 +3,33 @@ package org.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class DemoController {
 
-
     @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/user")
     public String home(Model model)  {
         User user = new User();
-        user.setName("name");
+        user.setName("");
         model.addAttribute(user);
-        return "home";
+        return "users";
     }
 
     @GetMapping("/newUser")
     public String newUser(User user) {
-        return "home";
+        return "users";
 
     }
 
+    @GetMapping("/info")
+    public String info() {
+        return "info";
+    }
 
 }
